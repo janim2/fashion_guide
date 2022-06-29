2ed790251d9daeabd99616aa497f8c889ec52760
Jesse <iamjesse75@gmail.com>
2022-06-29 12:25:11 +0000

    added sql file

--- database/sql/fashionguide1.sql
+++ database/sql/fashionguide1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 13, 2022 at 04:10 PM
+-- Generation Time: Jun 29, 2022 at 02:24 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -85,20 +85,50 @@ CREATE TABLE `clients` (
 --
 
 INSERT INTO `clients` (`client_id`, `customer_id`, `full_name`, `phone_number_1`, `phone_number_2`, `residential_address`, `email`, `gender`, `male_trouser_waist`, `male_trouser_seat`, `male_trouser_thigh`, `male_trouser_knee`, `male_trouser_bass`, `male_trouser_lenght`, `male_shirt_chest`, `male_shirt_back`, `male_shirt_short_sleeve`, `male_shirt_long_sleeve`, `male_shirt_around_arm`, `male_shirt_cuff`, `male_shirt_lenght`, `male_agbada_back`, `male_agbada_length`, `female_skirt_waist`, `female_skirt_hip`, `female_skirt_knee`, `female_skirt_waist_hip`, `female_skirt_waist_knee`, `female_skirt_skirt_lenght`, `female_dress_bust`, `female_dress_under_bust`, `female_dress_waist`, `female_dress_shoulder`, `female_dress_shoulder_nipple`, `female_dress_shoulder_under_bust`, `female_dress_shoulder_waist`, `female_dress_nipple_nipple`, `female_dress_shoulder_hip`, `female_dress_shoulder_knee`, `female_dress_top_lenght`, `female_dress_lenght`, `female_trouser_waist`, `female_trouser_seat`, `female_trouser_thigh`, `female_trouser_knee`, `female_trouser_bass`, `female_trouser_lenght`, `added_by`, `created_on`, `updated_on`) VALUES
-(12, 1, 'FADA', '34567890', '558479084', 'Kasoa Galilea', 'akorlirobert5@gmail.com', 'male', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, NULL, NULL),
-(13, 1, 'Aurelia Abbey', '4567890', '', 'Kasoa Galilea', 'akorlirobert5@gmail.com', 'male', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, NULL, NULL),
-(14, 1, 'forces', '6786789', '', 'Box 378 Kaneshie Accra', 'akorlirobert5@gmail.com', 'male', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, NULL, NULL),
-(15, 1, 'Handel', '234567890', '', 'Accra', 'fada@gmail.com', 'male', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, NULL, NULL),
-(16, 1, 'hi', '56880900999', '', 'dfghjk', '', 'male', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, NULL, NULL),
-(17, 3, 'Jesse', '12345689098765', '', 'Kasoa Galilea', 'akorlirobert5@gmail.com', 'male', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, NULL, NULL),
-(18, 1, 'Aurelia Abbey', '1234567890', '098765432', 'Kasoa', '', 'female', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, NULL, NULL),
-(19, 2, 'hmmm', '2345678', '234567', 'hmmm', '', 'male', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, NULL, NULL),
-(20, 1, 'Jesse Anim', '0268977129', '', 'Ghana', 'iamjesse75@gmail.com', 'male', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, NULL, NULL),
-(23, 1, 'Testing', '0268977129', '', 'plot 5, Airport Roundabout, Kumasi', 'animjesse55@gmail.com', 'male', '11', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, '2022-05-12 13:45:38', NULL),
-(24, 1, 'Testing 2', '0268977129', '', 'plot 5, Airport Roundabout, Kumasi', 'animjesse55@gmail.com', 'male', '2', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, '2022-05-12 13:49:23', NULL),
-(25, 1, 'Testing 3', '0268977129', '', 'plot 5, Airport Roundabout, Kumasi', 'animjesse55@gmail.com', 'male', '8', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, '2022-05-12 13:52:13', NULL),
-(26, 1, 'Testing 4', '0268977129', '', 'plot 5, Airport Roundabout, Kumasi', 'animjesse55@gmail.com', 'male', '11', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, '2022-05-12 13:54:10', NULL),
-(27, 1, 'Testing 5', '0268977129', '', 'plot 5, Airport Roundabout, Kumasi', 'animjesse55@gmail.com', 'male', '1', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, '2022-05-12 13:55:13', NULL);
+(30, 17, 'Jesse Anim', '0268977129', '', 'Ghana', 'animjesse55@gmail.com', 'male', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, '2022-06-23 16:04:42', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
+CREATE TABLE `company` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `logo` varchar(255) NOT NULL DEFAULT 'None',
+  `helpline` varchar(255) NOT NULL DEFAULT '1234567890',
+  `sms` int(11) NOT NULL DEFAULT 0,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `company`
+--
+
+INSERT INTO `company` (`id`, `name`, `location`, `logo`, `helpline`, `sms`, `date`) VALUES
+(26, 'Fashion School Incorporated', 'Kumasi', '7.jpg', '1234567890', 10, '2022-06-29 12:24:22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company_images`
+--
+
+CREATE TABLE `company_images` (
+  `id` int(11) NOT NULL,
+  `company_id` int(11) NOT NULL,
+  `image_url` varchar(255) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `company_images`
+--
+
+INSERT INTO `company_images` (`id`, `company_id`, `image_url`, `date`) VALUES
+(12, 26, '7.jpg', '2022-06-29 12:24:22');
 
 -- --------------------------------------------------------
 
@@ -130,15 +160,6 @@ CREATE TABLE `fabric_images` (
   `image_url` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `fabric_images`
---
-
-INSERT INTO `fabric_images` (`id`, `project_id`, `image_url`) VALUES
-(1, 28, 'lonely.jpeg'),
-(2, 28, 'list.png'),
-(3, 28, 'cart.png');
-
 -- --------------------------------------------------------
 
 --
@@ -157,18 +178,6 @@ CREATE TABLE `payments` (
   `days_to_complete` varchar(255) NOT NULL,
   `received_by` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `payments`
---
-
-INSERT INTO `payments` (`id`, `customer_id`, `client`, `project_id`, `amount_charged`, `balance`, `paying_amount`, `payment_date`, `days_to_complete`, `received_by`) VALUES
-(7, 1, '11', 0, '3000', '1500', '34', '2022-04-27', '0', '1'),
-(8, 1, '20', 13, '89', '', '20', '2022-05-12', '0', '1'),
-(9, 1, '20', 13, '89', '42', '3', '2022-05-12', '0', '1'),
-(10, 1, '20', 13, '89', '15', '30', '2022-05-12', '0', '1'),
-(11, 1, '20', 13, '89', '3', '33', '2022-05-12', '0', '1'),
-(12, 1, '20', 13, '89', '3', '33', '2022-05-12', '0', '1');
 
 -- --------------------------------------------------------
 
@@ -203,25 +212,7 @@ CREATE TABLE `projects` (
 --
 
 INSERT INTO `projects` (`project_id`, `customer_id`, `client`, `status`, `type_of_work`, `description`, `type_of_fabric`, `sewing_charges`, `delivery_charges`, `project_cost`, `advance_payment`, `balance`, `start_date`, `end_date`, `days_to_complete`, `mode_of_delivery`, `delivery_location`, `temp_img_upload_id`, `added_by`) VALUES
-(8, 1, '12', 1, 'mata', 'testing', 'Mahama', '', '', '200', '600', '400', '2022-02-11', '2022-02-25', '5', 'PickUp', '', 0, '1'),
-(9, 1, '11', 2, 'fadada', 'dfghjkjhgfdfghjk', 'cloth', '1000', '2000', '3000', '1500', '1500', '2022-03-31', '2022-04-03', '', 'Delivery', 'Ghana', 0, '1'),
-(10, 2, '19', 0, 'fadada', 'Jesse', 'kente', '1000', '230', '1230', '500', '730', '2022-04-13', '2022-04-16', '', 'Delivery', 'Ghana', 0, '2'),
-(13, 1, '20', 1, 'Shirt', 'A good work', 'Cotton', '3200', '2', '89', '44', '', '2022-04-19', '2022-04-21', '', 'Delivery', '', 0, '1'),
-(15, 1, '12', 0, 'Testing', 'I dont want to say', 'No way', '1000', '10', '1010', '0', '1010', '2022-05-12', '2022-06-03', '22', 'Delivery', 'Ghana', 0, '1'),
-(16, 1, '24', 0, 'Testing', 'Testing', 'No way', '1000', '0', '1000', '99', '901', '2022-05-12', '2022-05-25', '13', 'Delivery', 'Ghana', 0, '1'),
-(17, 1, '24', 1, 'Testing', 'Testing', 'No way', '1000', '10', '1010', '22', '988', '2022-05-12', '2022-05-06', '0', 'Delivery', 'Ghana', 0, '1'),
-(18, 1, '23', 0, 'Testing', 'Testing', 'No way', '1000', '0', '1000', '99', '901', '2022-05-12', '2022-06-07', '26', 'Delivery', 'Ghana', 0, '1'),
-(19, 1, '20', 0, 'Testing', 'Testing', 'No way', '333', '0', '333', '53', '280', '2022-05-12', '2022-05-12', '0', 'Delivery', 'Ghana', 0, '1'),
-(20, 1, '23', 0, 'Testing', 'Herer', 'No way', '333', '4', '337', '43', '294', '2022-05-13', '2022-06-01', '19', 'Delivery', 'Ghana', 580830, '1'),
-(21, 1, '16', 0, 'dsfds', 'dfd', 'Cotton', '1000', '3', '1003', '2', '1001', '2022-05-13', '2022-06-02', '20', 'Delivery', 'Ghana', 527650, '1'),
-(22, 1, '16', 0, 'sdfsd', 'sfsdfdf', 'No way', '1000', '20', '1020', '32', '988', '2022-05-13', '2022-05-18', '5', 'Delivery', 'Ghana', 451207, '1'),
-(23, 1, '16', 0, 'Testing', 'sdfds', 'sdfd', '100', '10', '110', '53', '57', '2022-05-13', '2022-06-01', '19', 'Delivery', 'Ghana', 64634, '1'),
-(24, 1, '16', 0, 'Testing', 'Ghana', 'No way', '200', '10', '210', '30', '180', '2022-05-13', '2022-06-02', '20', 'Delivery', 'Gana', 542607, '1'),
-(25, 1, '15', 0, 'Testing', 'gsdfdssd', 'dfds', '10', '20', '30', '53', '0', '2022-05-13', '2022-05-26', '13', 'Delivery', 'Ghana', 777372, '1'),
-(26, 1, '16', 0, 'Testing', 'Testing', 'sdfd', '333', '1', '334', '53', '281', '2022-05-13', '2022-05-25', '12', 'Delivery', 'Ghana', 288741, '1'),
-(27, 1, '20', 0, 'dsfds', 'Testing', 'Cotton', '1000', '10', '1010', '43', '967', '2022-05-13', '2022-05-27', '14', 'Delivery', 'Ghana', 16704, '1'),
-(28, 1, '24', 0, 'one', 'one', 'No way', '1000', '10', '1010', '53', '957', '2022-05-13', '2022-05-12', '0', 'Delivery', '20', 947725, '1'),
-(29, 1, '20', 0, 'Testing', '34242', 'No way', '3000', '0', '3000', '3', '2997', '2022-05-13', '2022-05-30', '17', 'Delivery', 'Ghana', 976874, '1');
+(30, 17, '30', 0, 'Slit and Kaba', '', 'Slit', '200', '10', '210', '0', '210', '2022-06-23', '2022-06-30', '7', 'Delivery', 'Kumasi', 618140, '17');
 
 -- --------------------------------------------------------
 
@@ -243,14 +234,18 @@ CREATE TABLE `sms` (
 --
 
 INSERT INTO `sms` (`id`, `customer_id`, `phone_number`, `message`, `credit`, `date`) VALUES
-(1, 0, '0268977129', 'Welcome Testing 5 to Fashion Guide. We are happy with your interest to do business with us.  Helpline: 0274756446.', 1, '2022-05-12 13:55:13'),
-(2, 1, '0268977129', 'Hi Testing 2, your Testing work has been added to our list of ongoing projects and expected to start on 2022-05-12. Sewing Cost is GHS 1000, Delivery Cost is GHS 10. Total project cost is GHS 1010, Advance payment made is GHS 22, Balance GHS 988. Your Testing will be completed in 0. Helpline: 0274756446.', 1, '2022-05-12 14:02:32'),
-(3, 1, '0268977129', 'Hi Jesse Anim, your Testing work has been added to our list of ongoing projects and expected to start on 2022-05-12. Sewing Cost is GHS 333, Delivery Cost is GHS 0. Total project cost is GHS 333, Advance payment made is GHS 53, Balance GHS 280. Your Testing will be completed in 0. Helpline: 0274756446.', 1, '2022-05-12 14:22:40'),
-(4, 1, '0268977129', 'Hi Testing 2 your Testing has been completed adn it\'s ready for Delivery. Kindly see to it that you have fully paid for the cost of project. Your Outstanding balance is GHC 1010.Kindly pass by our office to pickup your work. Helpline: 0274756446', 1, '2022-05-12 15:12:19'),
-(5, 1, '0268977129', 'Hi Jesse Anim your Shirt has been completed adn it\'s ready for Delivery. Kindly see to it that you have fully paid for the cost of project. Your Outstanding balance is GHC 0.A dispatch will call yo arrange for delivery at your location Ghana. Helpline 0274756446', 1, '2022-05-12 15:15:00'),
-(6, 1, '0268977129', 'one', 1, '2022-05-13 11:40:02'),
-(7, 1, '0268977129', 'One', 1, '2022-05-13 11:40:15'),
-(8, 1, '0268977129', 'Coconut', 1, '2022-05-13 11:42:17');
+(10, 17, '0268977129', 'Testing', 1, '2022-06-23 16:34:27'),
+(11, 17, '0268977129', 'Testing 2', 1, '2022-06-23 16:35:36'),
+(12, 17, '0268977129', 'Testing 3', 1, '2022-06-23 16:50:53'),
+(13, 17, '0268977129', 'Testing 3', 1, '2022-06-23 16:50:55'),
+(14, 17, '0268977129', 'Testing 3', 1, '2022-06-23 16:51:28'),
+(15, 17, '0268977129', 'Green', 1, '2022-06-23 16:53:17'),
+(16, 17, '0268977129', 'Hi Jesse Anim,the date to deliver your Slit and Kaba has been extended to 2022-06-20.Sorry for the inconvenience', 1, '2022-06-29 09:46:07'),
+(17, 17, '0268977129', 'Hi Jesse Anim,the date to deliver your Slit and Kaba has been extended to 2022-06-20.Sorry for the inconvenience', 1, '2022-06-29 09:47:20'),
+(18, 17, '0268977129', 'Hi Jesse Anim,the date to deliver your Slit and Kaba has been extended to 2022-06-22.Sorry for the inconvenience', 1, '2022-06-29 09:52:54'),
+(19, 17, '0268977129', 'Hi Jesse Anim,the date to deliver your Slit and Kaba has been extended to 2022-06-21.Sorry for the inconvenience', 1, '2022-06-29 09:54:07'),
+(20, 17, '0268977129', 'Hi Jesse Anim,the date to deliver your Slit and Kaba has been extended to 2022-06-30.Sorry for the inconvenience', 1, '2022-06-29 09:54:46'),
+(21, 17, '0268977129', 'Hi Jesse Anim,the date to deliver your Slit and Kaba has been extended to 2022-06-30.Sorry for the inconvenience', 1, '2022-06-29 09:57:31');
 
 -- --------------------------------------------------------
 
@@ -260,24 +255,22 @@ INSERT INTO `sms` (`id`, `customer_id`, `phone_number`, `message`, `credit`, `da
 
 CREATE TABLE `users` (
   `id` int(20) NOT NULL,
+  `company_id` int(11) NOT NULL,
   `full_name` varchar(255) NOT NULL,
-  `phone_number` varchar(100) NOT NULL,
+  `phone_number` varchar(100) NOT NULL DEFAULT '1234567890',
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `role` varchar(200) NOT NULL,
-  `sms` int(11) NOT NULL,
-  `created_on` datetime DEFAULT NULL,
-  `updated_on` datetime DEFAULT NULL
+  `role` varchar(200) NOT NULL DEFAULT 'Administrator',
+  `created_on` timestamp NULL DEFAULT current_timestamp(),
+  `updated_on` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `full_name`, `phone_number`, `username`, `password`, `role`, `sms`, `created_on`, `updated_on`) VALUES
-(1, 'SageIT Admin', '', 'admin', 'Welcome123', '', 0, '2022-01-08 16:28:41', '2022-01-08 16:28:41'),
-(2, 'Aurelia Abbey', '558479084', 'admin@sageitservices.com', 'welcome123', 'Administrator', 0, NULL, NULL),
-(3, 'Kobby', '1234567890', 'Kobby@gmail.com', '1234567890', 'staff', 0, NULL, NULL);
+INSERT INTO `users` (`id`, `company_id`, `full_name`, `phone_number`, `username`, `password`, `role`, `created_on`, `updated_on`) VALUES
+(17, 26, 'Jesse Anim', '1234567890', 'iamjesse75@gmail.com', '$2y$10$SlxCyHIY98OjEig3RnH2qOfPNenJQN1RllfUjh4Ak403MXxVZoMp2', 'Administrator', '2022-06-23 15:42:16', '2022-06-23 15:42:16');
 
 --
 -- Indexes for dumped tables
@@ -288,6 +281,18 @@ INSERT INTO `users` (`id`, `full_name`, `phone_number`, `username`, `password`, 
 --
 ALTER TABLE `clients`
   ADD PRIMARY KEY (`client_id`);
+
+--
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `company_images`
+--
+ALTER TABLE `company_images`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `customer`
@@ -333,7 +338,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `client_id` bigint(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `client_id` bigint(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `company`
+--
+ALTER TABLE `company`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `company_images`
+--
+ALTER TABLE `company_images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `customer`
@@ -357,19 +374,19 @@ ALTER TABLE `payments`
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `project_id` bigint(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `project_id` bigint(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `sms`
 --
 ALTER TABLE `sms`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
